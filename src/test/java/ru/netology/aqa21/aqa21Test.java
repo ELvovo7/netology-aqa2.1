86f0d87056b1f43ef4be118e041e0474bc3e80ed
Vladimir Kuzmin <vladimir_kyzmin@mail.ru>
2019-12-19 20:00:27 +0300

    v2

--- src/test/java/ru/netology/aqa21/aqa21Test.java
+++ src/test/java/ru/netology/aqa21/aqa21Test.java
@@ -1,23 +1,22 @@
 package ru.netology.aqa21;
 
 import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-
-public class aqa21Test {
-
+class FormTest {
     @Test
-    void shouldTest() {
-        open("http://localhost:9999");
+    void shouldSubmitForm(){
+        open("http://localhost:9999/");
 
-        $("[data-test-id=name] input").setValue("Хидирбек Джамалединов");
-        $("[data-test-id=phone] input").setValue("+79994596490");
-        $("[data-test-id=agreement]").click();
-        $(By.tagName("button")).click();
-        $("[data-test-id=order-success]").should(exist);
+        $(By.name("name")).setValue("Иван Разуваев");
+        $(By.cssSelector("[type = tel]")).setValue("+79995671111");
+        SelenideElement agriment = $("[data-test-id=agreement]");
+        agriment.click();
+        $(By.tagName("button")).submit();
+        $("[data-test-id=\"order-success\"]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
